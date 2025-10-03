b7190b9bd5c416a643f8f36a26676ab066eeb544
Hackk00 <jesuspolo00@gmail.com>
2025-10-03 16:10:55 -0500

    Editar y eliminar servicios

--- js/servicioDetalle.js
+++ js/servicioDetalle.js
@@ -13,15 +13,41 @@ $.getJSON("bd.json", function(data) {
             $(".significado").text(servicio.Significado);
             $(".beneficios").empty();
             $(".aplicacion").empty();
-
             servicio.Beneficios.forEach(beneficio => {
-              $(".beneficios").append('<li><i class="fas fa-check-circle text-black me-2"></i> &nbsp;' + beneficio + '</li>');
+                $(".beneficios").append('<li><i class="fas fa-check-circle text-black me-2"></i> &nbsp;' + beneficio + '</li>');
             });
-
             servicio.Aplicacion.forEach(aplicacion => {
-              $(".aplicacion").append('<li><i class="fas fa-check-circle text-black me-2"></i> &nbsp;' + aplicacion + '</li>');
+                $(".aplicacion").append('<li><i class="fas fa-check-circle text-black me-2"></i> &nbsp;' + aplicacion + '</li>');
             });
+        }
+    });
+});
+/*---------------------------------*/
+$.getJSON("bd.json", function(data) {
+    let servicios = data.Servicios;
+    let grupos = {};
+    servicios.forEach(servicio => {
+        if (id == servicio.id) {
+            $("#nombre").val(servicio.Nombre);
+            $("#slogan").val(servicio.slogan);
+            $("#descripcion").val(servicio.Descripcion);
+            $("#categoria").append($("<option></option>").val(servicio.Categoria) // valor del option
+                .text(servicio.Categoria) // texto que se verá en el select
+                .prop("selected", true) // marcarlo como seleccionado
+                );
+            $("#descripcion_corta").val(servicio.Descripcion);
+            $("#descripcion_larga").text(servicio.Significado);
+            /*--------------------------------------------*/
+            var beneficiosTexto = servicio.Beneficios.join(", ");
+            $("#beneficios").val(beneficiosTexto);
+            /*--------------------------------------------*/
+            var aplicacionTexto = servicio.Aplicacion.join(", ");
+            $("#aplicacion").val(aplicacionTexto);
 
+            $("#estado").append($("<option></option>").val(servicio.estado) // valor del option
+                .text(servicio.estado) // texto que se verá en el select
+                .prop("selected", true) // marcarlo como seleccionado
+                );
         }
     });
 });
--- js/serviciosAdmin.js
+++ js/serviciosAdmin.js
@@ -12,7 +12,7 @@ $.getJSON("bd.json", function(data) {
     let tbody = $("#tabla-servicios");
     servicios.forEach(servicio => {
         let fila = `
-            <tr>
+            <tr id="fila_${servicio.id}">
                 <td>${servicio.Nombre}</td>
                 <td>${servicio.Descripcion}</td>
                 <td>${servicio.Categoria}</td>
@@ -24,10 +24,10 @@ $.getJSON("bd.json", function(data) {
                 </td>
                 <td>
                     <div class="btn-group" role="group">
-                        <button class="btn btn-dark btn-sm" type="button" data-bs-toggle="tooltip" title="Editar">
+                        <a class="btn btn-dark btn-sm" href="editar_servicio.html?id=${servicio.id}" data-bs-toggle="tooltip" title="Editar">
                             <i class="fas fa-edit"></i>
-                        </button>
-                        <button class="btn btn-danger btn-sm" type="button" data-bs-toggle="tooltip" title="Eliminar">
+                        </a>
+                        <button class="btn btn-danger btn-sm" type="button" data-bs-toggle="tooltip" data-id="${servicio.id}" data-toggle="modal" data-target="#servicio_${servicio.id}" title="Eliminar">
                             <i class="fas fa-trash"></i>
                         </button>
                     </div>
@@ -38,4 +38,48 @@ $.getJSON("bd.json", function(data) {
     });
     // Inicializar tooltips de Bootstrap
     $('[data-bs-toggle="tooltip"]').tooltip();
+    servicios.forEach(servicio => {
+        $(".tabla_servicios").append(`
+            <div class="modal fade" id="servicio_${servicio.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-body">
+                            <div class="row p-2">
+                                <div class="col-lg-12 form-group text-center">
+                                    <h4 class="font-weight-bold text-dark mt-2">Deseas eliminar el servicio ${servicio.Nombre}?</h4>
+                                </div>
+                                <div class="col-lg-12 form-group text-center mt-3">
+                                    <button class="btn btn-danger btn-sm" type="button" data-dismiss="modal">
+                                        <i class="fas fa-times"></i>
+                                        &nbsp;
+                                        Cancelar
+                                    </button>
+                                    &nbsp;
+                                    <button class="btn btn-dark btn-sm confirmar" type="button">
+                                        <i class="fas fa-check"></i>
+                                        &nbsp;
+                                        Confirmar
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `);
+    });
+
+    $(document).on('click', '.confirmar', function() {
+    // Obtener el id del servicio desde el botón
+    const modal = $(this).closest('.modal');       // Modal actual
+    const servicioId = modal.attr('id').split('_')[1]; // extrae el id del modal: servicio_1 -> 1
+
+    // Hacer fade out a la fila correspondiente
+    $('#fila_' + servicioId).fadeOut(500, function() {
+        $(this).remove(); // opcional: eliminar del DOM después del fade
+    });
+
+    // Cerrar el modal
+    modal.modal('hide');
+});
 });
